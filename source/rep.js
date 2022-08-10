9a128d26ea8581c10c66512b20fc87eca39c930a
Sergio-Sousa-Sermio21 <yusermiodial21@gmail.com>
2022-08-10 11:24:56 +0100

    Repetitions

---
+++ source/rep.js
@@ -0,0 +1,15 @@
+var c = 0;
+while (c <= 3){
+    console.log(`${c}`)
+    c++;
+}
+
+//or
+c = 0
+do {
+    console.log(`${c}`)
+    c++;
+} while(c <= 3)
+var e = ['45', '2']
+for (i = 0; i < 2; i++)
+    console.log(e[i])

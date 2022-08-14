1e53a61676513e377ef67d1d04f306adc8b8da36
Sergio-Sousa-Sermio21 <yusermiodial21@gmail.com>
2022-08-14 11:44:00 +0100

    A little bit about array in js

--- source/rep.js
+++ source/rep.js
@@ -13,3 +13,16 @@ do {
 var e = ['45', '2']
 for (i = 0; i < 2; i++)
     console.log(e[i])
+
+//array
+var a = ['m','a','t','h']
+
+for (i = 0; i<a.length; i++)
+    console.log(a[i])
+
+//simplified (objects and arrays(which are objects))
+for (let i in a)
+    console.log(a[i])
+
+//serching a element of an array
+console.log(a.indexOf('a'))
